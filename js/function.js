6d0945618ea2ef712a9c3efaf11c20a2eca60c71
Kristina Piekutė <70894872+TINAZee@users.noreply.github.com>
2020-09-30 14:22:19 +0300

    file structure update

--- js/function.js
+++ js/function.js
@@ -1,8 +1,1 @@
 import posts from './data.js'
-import suma from './suma.js'
-import atimtis from './atimtis.js'
-import sayHi from './sayHi.js'
-
-console.log(sayHi("Rex"));
-
-console.log(atimtis(3,1));
