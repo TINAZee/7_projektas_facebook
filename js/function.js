ae82b86a3f7b5218743d15338adbdb42e1a495c7
Kristina Piekutė <70894872+TINAZee@users.noreply.github.com>
2020-09-30 14:20:44 +0300

    Initial commit

---
+++ js/function.js
@@ -0,0 +1,8 @@
+import posts from './data.js'
+import suma from './suma.js'
+import atimtis from './atimtis.js'
+import sayHi from './sayHi.js'
+
+console.log(sayHi("Rex"));
+
+console.log(atimtis(3,1));
